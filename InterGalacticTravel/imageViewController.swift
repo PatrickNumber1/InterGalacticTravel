6813a7a751117b8d198290f3d2d46bfb1120f14c
student3 <student3@jh-mbair-158-02.d214.org>
2015-11-09 14:16:47 -0600

    Create new ViewController and Segue

---
+++ InterGalacticTravel/imageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  imageViewController.swift
+//  InterGalacticTravel
+//
+//  Created by student3 on 11/9/15.
+//  Copyright © 2015 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class imageViewController: NSObject {
+
+}
+
+ 
+
